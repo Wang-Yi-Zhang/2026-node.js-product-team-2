2cec1ba2934526d73a3168dd66637bfcbb69a1d6
Ting <q.e.d14702@gmail.com>
2026-07-23 21:39:46 +0800

    feat：隨機檔名,計算壓縮值

--- src/utils/formatBytes.util.js
+++ src/utils/formatBytes.util.js
@@ -1,3 +1,9 @@
+/**
+ * 計算壓縮前後節省的值
+ * @param {number} originalSize - 原始檔案大小（bytes）
+ * @param {number} outputSize - 處理後檔案大小（bytes）
+ * @returns {number} - 節省的百分比，四捨五入到小數點後一位，例如 93.3
+ */
 function calculateSaved(originalSize, outputSize){
 
     // 計算壓縮後百分比
